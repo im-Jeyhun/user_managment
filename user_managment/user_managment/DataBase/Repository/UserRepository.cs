1764e4feedaf4bce5aefc6bd2cab0317f4ba1788
Ceyhun <contact.ceyhun@gmail.com>
2022-07-17 21:38:57 +0400

    initial structure

---
+++ user_managment/user_managment/DataBase/Repository/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_managment.DataBase.Repository
+{
+    internal class UserRepository
+    {
+    }
+}
---
+++ user_managment/user_managment/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace user_managment.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
