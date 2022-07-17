9e4e4a97a3b756da5844c9e7d6d856821d421337
Ceyhun <contact.ceyhun@gmail.com>
2022-07-17 21:47:50 +0400

    Validation class which contains base validators added to ApplicationLogic

---
+++ user_managment/user_managment/ApplicationLogic/Validations/Validation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace user_managment.ApplicationLogic.Validations
+{
+    internal class Validation
+    {
+        public static bool IsOnlyLetters(string text)
+        {
+            string patterms = "^[A-Z][a-zA-z]{3,30}$";
+            Regex regex = new Regex(patterms);
+            return regex.IsMatch(text);
+        }
+
+        public static bool IsEmailContains(string email)
+        {
+            string patterns = /*@"^[\w]{10,30}+[@]+[code]+[\.]+[edu]+[\.]+[az]+$"*/ @"^([a-zA-Z0-9]{10,30})(@code\.edu\.az)$";
+            Regex regexemail = new Regex(patterns);
+            return regexemail.IsMatch(email);
+        }
+
+        public static bool IsPasswordContains(string password)
+        {
+            string patterns = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+            Regex regexpassword = new Regex(patterns);
+            return regexpassword.IsMatch(password);
+        }
+    }
+}
