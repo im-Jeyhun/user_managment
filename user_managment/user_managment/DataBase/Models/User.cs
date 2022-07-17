68a73c16451e6ccab93e838d2933bff1dae4d444
Ceyhun <contact.ceyhun@gmail.com>
2022-07-17 21:42:19 +0400

    User model is completed

--- user_managment/user_managment/DataBase/Models/User.cs
+++ user_managment/user_managment/DataBase/Models/User.cs
@@ -8,5 +8,38 @@ namespace user_managment.DataBase.Models
 {
     internal class User
     {
+        private DateTime RegistrationDate { get; set; }
+
+        private static int idCounter = 1;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string name, string lastName, string email, string password)
+        {
+            Id = idCounter++;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            RegistrationDate = DateTime.Now;
+
+        }
+        public string GetUserInfo()
+        {
+            return $"Istifadeci adi : {Name} , soyadi : {LastName} , emaili : {Email} ";
+
+        }
+
+        public string GetUserInfoForAdmin()
+        {
+            return $"Istifadeci id : {Id} , Istifadeci adi : {Name} , soyadi : {LastName} , emaili : {Email} , qeydiyyat tarixi {RegistrationDate} ";
+
+        }
     }
 }
